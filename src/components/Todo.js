21677f1a5fd48b87b3167fccc191e30c6bb51cc5
wnipuna <nipuna.r3@gmail.com>
2021-07-04 03:22:14 +0530

    First commit

---
+++ src/components/Todo.js
@@ -0,0 +1,30 @@
+import {useState} from 'react';
+import Backdrop from './Backdrop';
+import Modal from './Modal';
+
+const Todo = (props) => {
+
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
+    const deleteHandler = () =>{
+        setModalIsOpen(true);
+    }
+
+    const closeModalHandler = () =>{
+        setModalIsOpen(false);
+    }
+
+    return <div className='card'>
+        <h2>{props.name}</h2>
+        <div className='actions'>
+            <button className='btn' onClick={deleteHandler}>{props.bname}</button>
+        </div>
+        {modalIsOpen && <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler}/>}
+        {modalIsOpen && <Backdrop onCancel={closeModalHandler}/>}
+    </div>
+}
+
+Todo.defaultProps = {
+    bname : "Delete"
+}
+export default Todo;
